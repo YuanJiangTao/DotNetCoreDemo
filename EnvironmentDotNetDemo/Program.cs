6de2de0cfbdb9d2620079a6c3067e35e3dca70e8
YuanJiangTao <yjt_cumtb@163.com>
2021-01-24 20:45:19 +0800

    添加项目文件。

---
+++ EnvironmentDotNetDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+namespace EnvironmentDotNetDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var env = hostContext.HostingEnvironment;
+                    if (env.IsDevelopment())
+                    {
+                        Console.WriteLine($"Development");
+                    }
+                    Console.WriteLine($"Hosting Environment:  + {env.EnvironmentName}");
+                });
+    }
+}
---
+++ SeqDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog.Configuration;
+using Serilog.Extensions.Logging;
+using Serilog;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace SeqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+        /*
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostBuilderContext,services) =>
+            {
+                services.AddLogging(loggingBuilder =>
+                {
+                    //此方法默认使用本地地址: 5341端口
+                    //加载Seq配置
+                    loggingBuilder.AddSeq(hostBuilderContext.Configuration.GetSection("Seq"));
+
+                });
+            });
+            */
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configuration) =>
+            {
+                configuration.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").
+                AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true).
+                AddEnvironmentVariables();
+            })
+            .UseSerilog((hostBuilderContext, loggingBuilder) =>
+        {
+            loggingBuilder.ReadFrom.Configuration(hostBuilderContext.Configuration);
+        }).ConfigureServices(services =>
+        {
+            services.AddHostedService<TestHostService>();
+        });
+    }
+}
